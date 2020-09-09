1132f710b3aa573f51809e04600787d1134a066e
Igor Souza <igor.ferreira.souza@gmail.com>
2020-09-09 18:06:45 -0300

    opa

--- yt-python/exercicio_1.py
+++ yt-python/exercicio_1.py
@@ -1,4 +1,8 @@
-
+#!/home/igor/.virtualenv/curso/bin/python
+import math
+import random
+import pygame
+import requests
 
 def desafio2():
 
@@ -20,6 +24,65 @@ def desafio3():
     print('Está em maiusculas? {}'.format(n3.isupper()))
     print('Está em minusculas?  {}'.format(n3.islower()))
     print('Está capitalizada? {}'.format(n3.istitle()))
+
+def desafio16():
+    n1 = float(input('Digite um número qualquer para saber sua porção inteira: '))
+    truncado = math.trunc(n1)
+    print('A porção inteira do número: {} é: {}'.format(n1,truncado))
+
+
+def desafio17():
+    cat1 = float(input('Cateto adjacente: '))
+    cat2 = float(input('Cat oposto: '))
+    hipo = math.hypot(cat1,cat2)
+    print('Hipotenusa = {:.2f}'.format(hipo))
+    # print('hipo2: {}'.format((cat1**2+cat2**2)**0.5))
+
+def desafio18():
+    ang = float(input('Digite o angulo: '))
+    sen = math.sin(math.radians(ang))
+    cos = math.cos(math.radians(ang))
+    tang = math.tan(math.radians(ang))
+    print('Seno: {:.2f} Cosseno: {:.2f} Tangente:{:.2f}'.format(sen,cos,tang))
+
+
+def desafio19():
+    lista=[]
+    i=1
+    while i<=4:
+        lista.append((input('Digite o nome do {} aluno: '.format(i))))
+        i=i+1
+        pass    
+    ganhador = random.randrange(1,4)
+    print('O ganhador é: {}'.format(lista[ganhador]))
+
+def desafio20():
+    lista=[]
+    # listaOrdem=[]
+    i=1
+    while i<=4:
+        lista.append((input('Digite o nome do aluno {}: '.format(i))))                
+        i=i+1
+        pass
+
+    random.shuffle(lista)
+    # while len(listaOrdem)<4:
+    #     r = random.randint(1,4)
+    #     if r not in listaOrdem:
+    #         listaOrdem.append(r)
+    #     pass
+    for idx,lo in enumerate(lista):        
+        print('A ordem será: {}º : {}'.format(idx+1,lo))
+
+
+def desafio21():
+    pygame.init()
+    pygame.mixer.music.load('i.mp3')
+    pygame.mixer.music.play()
+    pygame.event.wait()
+
+desafio21()
+
 
 
 # desafio3()
